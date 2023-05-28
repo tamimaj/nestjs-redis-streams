302573807d79d113899d745518ffda83a7e9e0ca
tamimaj <tamimabbasaljuratli@gmail.com>
2023-05-28 02:13:45 +0200

    Added demo code in the examples.

--- examples/client-app/src/app.controller.ts
+++ examples/client-app/src/app.controller.ts
@@ -1,13 +1,49 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { lastValueFrom } from 'rxjs';
+import { RedisStreamClient } from '@tamimaj/nestjs-redis-streams';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly redisStreamClient: RedisStreamClient) {}
 
   @Get()
-  getHello(): string {
-    console.log('controller got called.');
-    return this.appService.getHello();
+  async getHello(): Promise<any> {
+    // send a message and get a response.
+
+    const observable = this.redisStreamClient.send('users:create', {
+      data: { name: 'tamim' }, // this will be the data of the stream, and will be serialized to JSON,
+      // if you use the default serializer.
+
+      // correlationId: '1234', // override the correlationId, if you want to use your own. Default is uuid.
+      // will be used to map the response to the correct observer.
+
+      anyOtherHeadersKey: 'anyOtherHeadersValue', // any other headers you want to add to the stream.
+      // headers are key value pairs, and will NOT be serialized to JSON, if you use the default serializer.
+    });
+
+    const response = await lastValueFrom(observable); // get the last value from the observable.
+
+    console.log('response from the stream: ', response);
+
+    return JSON.stringify(response);
+  }
+
+  @Get('/emit')
+  async getHelloEmit(): Promise<any> {
+    // emit a message and don't wait for a response.
+    // fire and forget.
+
+    const observable = this.redisStreamClient.emit('users:create', {
+      data: { name: 'tamim', fireAndForgetEvent: true }, // this will be the data of the stream, and will be serialized to JSON,
+      // if you use the default serializer.
+
+      // correlationId: '1234', // override the correlationId, if you want to use your own. Default is uuid.
+      // will be used to map the response to the correct observer.
+
+      anyOtherHeadersKey: 'anyOtherHeadersValue', // any other headers you want to add to the stream.
+      // headers are key value pairs, and will NOT be serialized to JSON, if you use the default serializer.
+    });
+
+    return 'Message Sent.';
   }
 }
--- examples/client-app/src/app.module.ts
+++ examples/client-app/src/app.module.ts
@@ -1,9 +1,57 @@
 import { Module } from '@nestjs/common';
+import {
+  ClientConstructorOptions,
+  RedisStreamClientModule,
+} from '@tamimaj/nestjs-redis-streams';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Any class that implements createRedisStreamClientModuleOptions method.
+// ConfigService is a good candidate.
+// to be used with registerAsync, useClass.
+class ClassOptions {
+  createRedisStreamClientModuleOptions(): ClientConstructorOptions {
+    return {
+      streams: { consumer: 'api-1', block: 5000, consumerGroup: 'api' },
+      connection: { url: '0.0.0.0:6379' },
+      responseStreams: ['users:created', 'users:created:copy'],
+    };
+  }
+}
+
 @Module({
-  imports: [],
+  imports: [
+    // Register / forRoot.
+    RedisStreamClientModule.register({
+      connection: { url: '0.0.0.0:6379' },
+      streams: { consumer: 'api-1', block: 5000, consumerGroup: 'api' },
+      responseStreams: ['users:created', 'users:created:copy'],
+    }),
+
+    // registerAsync / forRootAsync.
+
+    //////////////////////
+    // Use Factory.     //
+    //////////////////////
+
+    // RedisStreamClientModule.registerAsync({
+    //   useFactory: async () => {
+    //     return {
+    //       connection: { url: '0.0.0.0:6379' },
+    //       streams: { consumer: 'api-1', block: 5000, consumerGroup: 'api' },
+    //       responseStreams: ['users:created', 'users:created:copy'],
+    //     };
+    //   },
+    // }),
+
+    //////////////////////
+    // Use Class.       //
+    //////////////////////
+
+    // RedisStreamClientModule.forRootAsync({
+    //   useClass: ClassOptions,
+    // }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
